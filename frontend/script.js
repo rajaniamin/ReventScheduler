8cdb7d7d16797dc86819de9659c483dccdf246b7
Rajani VC <112753940+rajaniamin@users.noreply.github.com>
2023-10-12 20:20:45 +0530

    done

--- frontend/script.js
+++ frontend/script.js
@@ -1,192 +1,134 @@
 document.addEventListener("DOMContentLoaded", function () {
-  
-
-  // Get a reference to the input field
+  const apiUrl = "http://localhost:8080";
   const newTaskInput = document.getElementById("new-task-input");
-
-  // Get a reference to the element where you want to display the tasks
   const tasksContainer = document.getElementById("tasks");
 
-  // Listen for the form submission event
+  function createTaskElement(taskData) {
+    const taskDiv = document.createElement("div");
+    taskDiv.className = "task";
+
+    const taskContentDiv = document.createElement("div");
+    taskContentDiv.className = "content";
+
+    const taskInput = document.createElement("input");
+    taskInput.type = "text";
+    taskInput.className = "text";
+    taskInput.value = taskData.name;
+    taskInput.readOnly = true;
+
+    taskContentDiv.appendChild(taskInput);
+
+    const actionsDiv = document.createElement("div");
+    actionsDiv.className = "actions";
+
+    const editButton = document.createElement("button");
+    editButton.className = "edit";
+    editButton.innerText = "Edit";
+
+    const deleteButton = document.createElement("button");
+    deleteButton.className = "delete";
+    deleteButton.innerText = "Delete";
+
+    actionsDiv.appendChild(editButton);
+    actionsDiv.appendChild(deleteButton);
+
+    taskDiv.appendChild(taskContentDiv);
+    taskDiv.appendChild(actionsDiv);
+
+let isEditing = false;
+
+editButton.addEventListener('click', () => {
+  if (!isEditing) {
+    isEditing = true;
+    editButton.innerText = "Save";
+    taskInput.removeAttribute("readonly");
+    taskInput.focus();
+  } else {
+    isEditing = false;
+    editButton.innerText = "Edit";
+    taskInput.setAttribute("readonly", "readonly");
+    updateTask(taskData.id, taskInput.value);
+  }
+});
+
+
+    deleteButton.addEventListener('click', () => {
+      tasksContainer.removeChild(taskDiv);
+      deleteTask(taskData.id);
+    });
+
+    return taskDiv;
+  }
+
+  function addTask(task) {
+    const taskDiv = createTaskElement(task);
+    tasksContainer.appendChild(taskDiv);
+  }
+
+  function updateTask(taskId, newName) {
+    return fetch(`${apiUrl}/update`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ id: taskId, name: newName }),
+    })
+      .then(response => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return response.json();
+      })
+      .then(data => {
+        console.log('Task updated:', data);
+        return data;
+      })
+      .catch(error => {
+        console.error('Error updating task:', error);
+        throw error;
+      });
+  }
+
+  function deleteTask(taskId) {
+    return fetch(`${apiUrl}/delete/${taskId}`, {
+      method: 'DELETE',
+    })
+      .then(response => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        console.log('Task deleted from the server');
+      })
+      .catch(error => {
+        console.error('Error deleting task from the server:', error);
+      });
+  }
+
   document.getElementById("new-task-form").addEventListener("submit", function (event) {
-      event.preventDefault(); // Prevent the default form submission
+    event.preventDefault();
+    const newTaskName = newTaskInput.value;
+    const newTask = { name: newTaskName };
 
-      // Get the value from the input field
-      const newTaskName = newTaskInput.value;
-
-      // Define the URL of your API endpoint
-      const apiUrl = "http://localhost:8080/add"; 
-
-      // Create the new task object
-      const newTask = {
-          name: newTaskName,
-      };
-
-      // Fetch data from the API using a POST request
-      fetch(apiUrl, {
-          method: "POST",
-          headers: {
-              "Content-Type": "application/json",
-          },
-          body: JSON.stringify(newTask),
+    fetch(`${apiUrl}/add`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newTask),
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return response.json();
       })
-          .then((response) => {
-              if (!response.ok) {
-                  throw new Error("Network response was not ok");
-              }
-              return response.json();
-          })
-          .then((data) => {
-              // Process the data 
-              console.log(data);
-
-              // Clear the input field after adding the task
-              newTaskInput.value = "";
-
-              // Create a new task element and add it to the tasksContainer
-              const taskDiv = document.createElement("div");
-              taskDiv.className = "task";
-
-              const taskContentDiv = document.createElement("div");
-              taskContentDiv.className = "content";
-
-              const taskInput = document.createElement("input");
-              taskInput.type = "text";
-              taskInput.className = "text";
-              taskInput.value = data.name; // Use the data from the response
-              taskInput.readOnly = true;
-
-              taskContentDiv.appendChild(taskInput);
-
-              const actionsDiv = document.createElement("div");
-              actionsDiv.className = "actions";
-
-              const editButton = document.createElement("button");
-              editButton.className = "edit";
-              editButton.innerText = "Edit";
-
-              const deleteButton = document.createElement("button");
-              deleteButton.className = "delete";
-              deleteButton.innerText = "Delete";
-
-              actionsDiv.appendChild(editButton);
-              actionsDiv.appendChild(deleteButton);
-
-              taskDiv.appendChild(taskContentDiv);
-              taskDiv.appendChild(actionsDiv);
-
-              tasksContainer.appendChild(taskDiv);
-          })
-          .catch((error) => {
-              console.error("Error fetching data:", error);
-          });
-  });
-});
-
-
-document.addEventListener("DOMContentLoaded", function () {
-  
-  // Function to delete a task from the database and the webpage
-  function deleteTask(id, taskElement) {
-      // Define the URL of your API endpoint for deleting a task
-      const deleteUrl = `http://localhost:8080/delete/${id}`; 
-
-      // Send a DELETE request to the server
-      fetch(deleteUrl, {
-          method: "DELETE",
+      .then((data) => {
+        console.log(data);
+        newTaskInput.value = "";
+        addTask(data);
       })
-          .then((response) => {
-              if (!response.ok) {
-                  throw new Error("Network response was not ok");
-              }
-
-              // Remove the task element from the webpage
-              taskElement.remove();
-          })
-          .catch((error) => {
-              console.error("Error deleting task:", error);
-          });
-  }
-
-  // Iterate through existing tasks and attach delete event listeners
-  const existingTasks = document.querySelectorAll(".task");
-  existingTasks.forEach((taskDiv) => {
-      const taskId = taskDiv.getAttribute("data-task-id"); // Assuming you have a data attribute for task ID
-      taskDiv.querySelector(".delete").addEventListener("click", function () {
-          deleteTask(taskId, taskDiv);
+      .catch((error) => {
+        console.error("Error fetching data:", error);
       });
   });
-
-});
-
-
-
-
-document.addEventListener("DOMContentLoaded", function () {
-  // ...
-
-  // Function to edit a task
-  function editTask(taskInput, taskDiv) {
-      taskInput.readOnly = false;
-      taskInput.focus(); // Focus on the input for editing
-
-      // Hide the "Edit" button and show the "Save" button
-      taskDiv.querySelector(".edit").style.display = "none";
-      taskDiv.querySelector(".save").style.display = "block";
-  }
-
-  // Function to save changes to a task
-  function saveTask(taskInput, taskId, taskDiv) {
-      taskInput.readOnly = true;
-
-      // Get the updated task description
-      const updatedTaskDescription = taskInput.value;
-
-      // Define the URL of your API endpoint for updating a task
-      const updateUrl = `http://localhost:8080/update`; 
-
-      // Create the updated task object
-      const updatedTask = {
-          id: taskId,
-          name: updatedTaskDescription,
-      };
-
-      // Fetch data from the API using a PUT request
-      fetch(updateUrl, {
-          method: "PUT",
-          headers: {
-              "Content-Type": "application/json",
-          },
-          body: JSON.stringify(updatedTask),
-      })
-          .then((response) => {
-              if (!response.ok) {
-                  throw new Error("Network response was not ok");
-              }
-
-              // Show the "Edit" button and hide the "Save" button
-              taskDiv.querySelector(".edit").style.display = "block";
-              taskDiv.querySelector(".save").style.display = "none";
-          })
-          .catch((error) => {
-              console.error("Error updating task:", error);
-          });
-  }
-
-  // Iterate through existing tasks and attach edit and save event listeners
-  const existingTasks = document.querySelectorAll(".task");
-  existingTasks.forEach((taskDiv) => {
-      const taskInput = taskDiv.querySelector(".text");
-      const taskId = taskDiv.getAttribute("data-task-id");
-
-      taskDiv.querySelector(".edit").addEventListener("click", function () {
-          editTask(taskInput, taskDiv);
-      });
-
-      taskDiv.querySelector(".save").addEventListener("click", function () {
-          saveTask(taskInput, taskId, taskDiv);
-      });
-  });
-
-  // ...
 });
