64ce322ecb8d72c8bd236c04620af90043e5116c
Rajani VC <112753940+rajaniamin@users.noreply.github.com>
2023-10-12 16:56:04 +0530

    done

--- frontend/script.js
+++ frontend/script.js
@@ -111,9 +111,82 @@ document.addEventListener("DOMContentLoaded", function () {
   // Iterate through existing tasks and attach delete event listeners
   const existingTasks = document.querySelectorAll(".task");
   existingTasks.forEach((taskDiv) => {
+      const taskId = taskDiv.getAttribute("data-task-id"); // Assuming you have a data attribute for task ID
       taskDiv.querySelector(".delete").addEventListener("click", function () {
-          deleteTask(data.id, taskDiv);
+          deleteTask(taskId, taskDiv);
       });
   });
 
+});
+
+
+
+
+document.addEventListener("DOMContentLoaded", function () {
+  // ...
+
+  // Function to edit a task
+  function editTask(taskInput, taskDiv) {
+      taskInput.readOnly = false;
+      taskInput.focus(); // Focus on the input for editing
+
+      // Hide the "Edit" button and show the "Save" button
+      taskDiv.querySelector(".edit").style.display = "none";
+      taskDiv.querySelector(".save").style.display = "block";
+  }
+
+  // Function to save changes to a task
+  function saveTask(taskInput, taskId, taskDiv) {
+      taskInput.readOnly = true;
+
+      // Get the updated task description
+      const updatedTaskDescription = taskInput.value;
+
+      // Define the URL of your API endpoint for updating a task
+      const updateUrl = `http://localhost:8080/update`; 
+
+      // Create the updated task object
+      const updatedTask = {
+          id: taskId,
+          name: updatedTaskDescription,
+      };
+
+      // Fetch data from the API using a PUT request
+      fetch(updateUrl, {
+          method: "PUT",
+          headers: {
+              "Content-Type": "application/json",
+          },
+          body: JSON.stringify(updatedTask),
+      })
+          .then((response) => {
+              if (!response.ok) {
+                  throw new Error("Network response was not ok");
+              }
+
+              // Show the "Edit" button and hide the "Save" button
+              taskDiv.querySelector(".edit").style.display = "block";
+              taskDiv.querySelector(".save").style.display = "none";
+          })
+          .catch((error) => {
+              console.error("Error updating task:", error);
+          });
+  }
+
+  // Iterate through existing tasks and attach edit and save event listeners
+  const existingTasks = document.querySelectorAll(".task");
+  existingTasks.forEach((taskDiv) => {
+      const taskInput = taskDiv.querySelector(".text");
+      const taskId = taskDiv.getAttribute("data-task-id");
+
+      taskDiv.querySelector(".edit").addEventListener("click", function () {
+          editTask(taskInput, taskDiv);
+      });
+
+      taskDiv.querySelector(".save").addEventListener("click", function () {
+          saveTask(taskInput, taskId, taskDiv);
+      });
+  });
+
+  // ...
 });
